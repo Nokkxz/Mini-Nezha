ab27914007e8491f79c2de94f4d9bcb6995c7d8c
halo <halo@halo.localdomain>
2020-11-16 11:18:04 +0800

    usb2can

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/halo/workspace/mini-nezha
+# Build directory: /home/halo/workspace/mini-nezha/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

49d31a0d149403408e767e3bed7e0f8345205753
Nokk <1405371901@qq.com>
2021-01-15 17:28:07 +0800

    .gitignore update

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/halo/workspace/mini-nezha
-# Build directory: /home/halo/workspace/mini-nezha/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
